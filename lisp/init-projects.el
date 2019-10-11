2f16f55427073bedbf142a7c2cc86d318fad8f51
firstrow <firstrow@gmail.com>
2019-10-11 14:47:28 +0200

    fixed projectile config

--- lisp/init-projects.el
+++ lisp/init-projects.el
@@ -1,21 +1,20 @@
 (use-package persp-mode
   :config
-  (with-eval-after-load "persp-mode-autoloads"
-    (setq wg-morph-on nil) ;; switch off animation
-    (setq persp-auto-resume-time 0)
-    (add-hook 'after-init-hook #'(lambda () (persp-mode 1)))
+  (setq wg-morph-on nil) ;; switch off animation
+  (setq persp-auto-resume-time 0)
+  (add-hook 'after-init-hook #'(lambda () (persp-mode 1)))
 
-    (setq persp-nil-name "home")
+  (setq persp-nil-name "home")
 
-    (ryo-modal-key
-     "SPC l" :hydra
-     '(hydra-layout ()
-                    "Layout"
-                    ("s" persp-frame-switch "switch project")
-                    ("n" persp-next "next")
-                    ("p" persp-prev "prev")
-                    ("r" persp-rename "rename")
-                    ("a" persp-add-new "add new")
-                    ("q" nil "cancel" :color blue)))))
+  (ryo-modal-key
+   "SPC l" :hydra
+   '(hydra-layout ()
+                  "Layout"
+                  ("s" persp-frame-switch "switch project")
+                  ("n" persp-next "next")
+                  ("p" persp-prev "prev")
+                  ("r" persp-rename "rename")
+                  ("a" persp-add-new "add new")
+                  ("q" nil "cancel" :color blue))))
 
 (provide 'init-projects)
